1f07e5c12066b23f3c4f2e4325563803acf0f3c6
LauLeysen <laurenceleysen5@gmail.com>
2024-12-13 11:46:56 +0100

    test commit vuln

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import os
 import requests
-from dotenv import load_dotenv
 
-# pul
-
-# Laad de variabelen uit het .env bestand
-load_dotenv()
 
 # Haal SECRET_KEY op uit de omgevingsvariabelen
-SECRET_KEY = os.getenv("SECRET_KEY")
 SECRET_KEY="TEST"
 
 def greet():
